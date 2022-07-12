704ebce0878acd259cf0b8b2c5c928eebb80e55c
Afif Patel <afifpatel@hotmail.co.uk>
2022-07-12 18:28:42 +0100

    initial commit

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -3,6 +3,6 @@ var personName = "Afif Patel";
 var pets = 2;
 var funFact = "I have Covid";
 
-console.log(personName);
-console.log(pets);
+console.log("my name is " + personName);
+console.log("the number of pets i have is " + pets);
 console.log(funFact);
