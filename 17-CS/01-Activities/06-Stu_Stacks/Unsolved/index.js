c32a590d569c77466a0798d0522fdd56db0eca32
Afif Patel <afifpatel@hotmail.co.uk>
2022-10-25 18:29:15 +0100

    1

---
+++ 17-CS/01-Activities/06-Stu_Stacks/Unsolved/index.js
@@ -0,0 +1,4 @@
+// create the Stack class with two methods to remove and add an element
+class Stack {}
+
+module.exports = Stack;
---
+++ 17-CS/01-Activities/10-Stu_Callback-Queue/Unsolved/assets/js/script.js
@@ -0,0 +1,5 @@
+// function to block the stack for x number of millise
+setTimeout(() => {
+  // remove the class that hides the element
+  document.querySelector("form-input").classList.remove("display-none");
+}, 8000);
